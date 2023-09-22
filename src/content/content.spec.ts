b2eb75427fdb68b25ff5253464f1ab09cc734da3
Leonardo Ferreira Lima <80768424@redecorp.br>
2023-09-22 15:08:01 -0300

    test: add missing test case on get all contents

--- src/content/content.spec.ts
+++ src/content/content.spec.ts
@@ -11,6 +11,11 @@ const linkHeader =
   '<https://www.tabnews.com.br/api/v1/contents?strategy=relevant&page=2&per_page=30>; rel="next", ' +
   '<https://www.tabnews.com.br/api/v1/contents?strategy=relevant&page=6&per_page=30>; rel="last"';
 
+const lastPageLinkHeader =
+  '<https://www.tabnews.com.br/api/v1/contents?strategy=relevant&page=1&per_page=30>; rel="first", ' +
+  '<https://www.tabnews.com.br/api/v1/contents?strategy=relevant&page=5&per_page=30>; rel="prev", ' +
+  '<https://www.tabnews.com.br/api/v1/contents?strategy=relevant&page=6&per_page=30>; rel="last"';
+
 describe('Content', () => {
   beforeEach(() => {
     tabNews = new TabNews({
@@ -64,6 +69,67 @@ describe('Content', () => {
         last_page: 6,
         next_page: 2,
         previous_page: undefined,
+        total_rows: 175,
+      });
+
+      expect(response.contents).toMatchObject([
+        {
+          id: 'id',
+          owner_id: 'owner_id',
+          parent_id: null,
+          slug: 'slug',
+          title: 'title',
+          status: 'published',
+          source_url: 'https://source.url.com/source',
+          created_at: new Date('2023-09-19T12:16:04.812Z'),
+          updated_at: new Date('2023-09-19T12:16:04.812Z'),
+          published_at: new Date('2023-09-19T12:16:04.837Z'),
+          deleted_at: null,
+          tabcoins: 1,
+          owner_username: 'username',
+          children_deep_count: 2,
+        },
+      ]);
+    });
+
+    it('should return correct page when is last page', async () => {
+      fetchMock.mockOnce(
+        JSON.stringify([
+          {
+            id: 'id',
+            owner_id: 'owner_id',
+            parent_id: null,
+            slug: 'slug',
+            title: 'title',
+            status: 'published',
+            source_url: 'https://source.url.com/source',
+            created_at: '2023-09-19T12:16:04.812Z',
+            updated_at: '2023-09-19T12:16:04.812Z',
+            published_at: '2023-09-19T12:16:04.837Z',
+            deleted_at: null,
+            tabcoins: 1,
+            owner_username: 'username',
+            children_deep_count: 2,
+          },
+        ]),
+        {
+          headers: {
+            link: lastPageLinkHeader,
+            'x-pagination-total-rows': '175',
+          },
+        },
+      );
+
+      const response = await tabNews.content.getAll();
+
+      expect(response.pagination).toMatchObject({
+        strategy: 'relevant',
+        page: 6,
+        per_page: 30,
+        first_page: 1,
+        last_page: 6,
+        next_page: undefined,
+        previous_page: 5,
         total_rows: 175,
       });
 
